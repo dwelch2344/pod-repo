09df75384f1a19813e37622e7ef919380b63819f
Dave Welch <dave.welch@mokinetworks.com>
2013-10-22 15:37:10 -0600

    Initial commit

---
+++ ExampleCoreLib/0.0.1/ExampleCoreLib/FooService.h
@@ -0,0 +1,15 @@
+//
+//  FooService.h
+//  ExampleCoreLib
+//
+//  Created by Dave Welch on 10/22/13.
+//  Copyright (c) 2013 David Welch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol FooService <NSObject>
+
+- (NSString*) getMessage;
+
+@end
